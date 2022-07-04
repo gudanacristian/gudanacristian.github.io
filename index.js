600ed95bd8ccff5d3e09bf76549cd0236f8f0c15
Guana Cristian Bogdan <gudanacristian@gmail.com>
2022-07-04 21:00:34 +0300

    first version of cv

---
+++ index.js
@@ -0,0 +1,23 @@
+// console.info(2);
+// console.debug(23 + 2);
+// console.warn(6 * 3 - 10);
+// console.error("started");
+
+var myName = "Cristian";
+var age = 19;
+
+// console.info("My name is " + myName + ", I'm " + age + " years old.");
+age = 30;
+// console.info("My name is " + myName + ", I'm " + age + " years old.");
+age = 31;
+// console.info("My name is " + myName + ", I'm " + age + " years old.");
+
+var jobTitle = "<span>Web Student</span> @ FastTrackIT";
+var mottoElement = document.getElementById("motto");
+
+console.info(mottoElement);
+console.warn(mottoElement.innerHTML);
+
+// mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
+
+mottoElement.innerHTML += " & " + jobTitle;
